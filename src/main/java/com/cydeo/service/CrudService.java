770f01fc7b7433d5f29ca58b4ecb9a0c0e59caee
gokhan.guzelyurt <Gok26han@>
2022-03-28 18:14:46 +0200

    Add role

---
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+import java.util.List;
+
+public interface CrudService <T,ID>{
+// we create the generic clas instead of  role and user service
+
+    T save(T object);
+     List<T> findAll();
+     T findById(ID id);
+     void deleteById(ID id);
+}
---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,18 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+
+import java.util.List;
+
+public interface RoleService  extends CrudService<RoleDTO,Long>{
+
+//    RoleDTO save(RoleDTO role);
+//    RoleDTO findByID(Long id);
+//    List<RoleDTO> findAll();
+//     void delete(RoleDTO role);
+//     void deleteById(Long id);
+
+
+}
